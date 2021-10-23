3f48b9e4033482515ed41dd2d123816a57b06cd6
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2021-10-23 18:23:37 +0530

    solved allowed host

--- crud/settings.py
+++ crud/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-4ev)ajg9n74v#xine^u5jo+eudgokzl%u2vos!%ax8k!o^+#sv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mycruddjangoapp.herokuapp.com']
 
 
 # Application definition
