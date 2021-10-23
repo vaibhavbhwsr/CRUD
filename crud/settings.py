329d1810991cd8a4b8038ba1dc2fb3bd980fb5a0
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2021-10-23 18:43:12 +0530

    updated STATIC_ROOT

--- crud/settings.py
+++ crud/settings.py
@@ -125,9 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [Path(BASE_DIR, "staticfiles"), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
