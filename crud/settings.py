d405172391797867d3f2ffc694f47f017380ff47
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2021-10-24 11:16:19 +0530

    Modified django_on_heroku

--- crud/settings.py
+++ crud/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import sys
 import os
 from pathlib import Path
-import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,4 +143,4 @@ REST_FRAMEWORK = {
 }
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
