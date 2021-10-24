bf1dde71e6668885e15e95dc6dd1ed23995b0193
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2021-10-24 11:15:05 +0530

    Modified settings for django-heroku

--- crud/settings.py
+++ crud/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import sys
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +141,6 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     )
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
